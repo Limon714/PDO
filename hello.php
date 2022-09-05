51508e89d3e8ea420be6cc95c3205bd7799665b8
Limon714 <limon4u714@gmail.com>
2022-09-05 14:00:48 +0600

    first

---
+++ hello.php
@@ -0,0 +1,33 @@
+<?php 
+
+$db_name = "mysql:host=localhost;dbname=person";
+$username = "root";
+$password = "";
+
+$conn = new PDO($db_name, $username, $password);
+$sql = $conn->query("SELECT * FROM user ");
+
+$result = $sql->fetchAll(PDO::FETCH_NUM);
+//    echo "<pre>";
+//    print_r($result);
+//    echo "</pre>";
+
+if(count($result)){
+    foreach($result as $row){
+        echo "{$row[0]} - {$row[1]} - {$row[2]} - {$row[3]} <br>";
+    }
+}
+   
+// while($row = $sql->fetch(PDO::FETCH_OBJ)){
+//     echo "{$row->ID} - {$row->Name} - {$row->City} - {$row->DOB} </br>";
+//     // echo "{$row[0]} - {$row[1]} - {$row[2]} - {$row[3]} </br>";
+    
+//     // echo "<pre>";
+//     // print_r($row);
+//     // echo "</pre>";
+    
+// }
+
+
+
+?>
